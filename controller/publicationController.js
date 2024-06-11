464411f58e81a2c2ea8f915657344753a4a815bd
Alsu Kurbanalieva <alsukurbanalieva035@gmail.com>
2024-06-11 12:19:26 +0300

    CreatePublication: add creative tag, file, publication

--- controller/publicationController.js
+++ controller/publicationController.js
@@ -6,7 +6,7 @@ class PublicationController {
   async createPublication(req, res) {
     try {
       const userId = req.userId
-      const {title, content, description, price, ageLimitId, tags} = req.body
+      const {title, content, description, price, ageLimitId, tags, typeFileId} = req.body
       const {file} = req.files
       const publication = await Publication.create({
         title, content, description, price, ageLimitId, userId, statusOfPublicationId: 1
@@ -15,9 +15,18 @@ class PublicationController {
         console.log(tag.id)
         await Publication_tag.create({creativeTagId: tag.id, publicationId: publication.id})
       })
-      let fileName = uuid.v4()
-      file.mv(path.resolve(__dirname, '..', 'static', fileName))
-      const file2 = await File.create({name: fileName, typeFileId: 1, userId, approve: true,})
+      let fileName
+      if(parseInt(typeFileId) === 1) {
+        fileName = uuid.v4() + '.mp4'
+      }
+      if(parseInt(typeFileId) === 2) {
+        fileName = uuid.v4() + '.mp3'
+      }
+      if(parseInt(typeFileId) === 3) {
+        fileName = uuid.v4() + '.jpg'
+      }
+      await file.mv(path.resolve(__dirname, '..', 'static', fileName))
+      const file2 = await File.create({name: fileName, typeFileId, userId, approve: true,})
       return res.json('Добавлено')
     } catch (e) {
       console.log(e.message)
