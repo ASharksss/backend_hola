b7eb1cd3600b224f234b4240f446abe735c77499
FOLLOO <nafis.safiullin2004@list.ru>
2024-12-20 17:00:23 +0300

    Фотки seedrs

--- seeders/seed6-type_files.js
+++ seeders/seed6-type_files.js
@@ -1,37 +1,42 @@
-
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('type_files', [
-  {
-    "id": 1,
-    "name": "Обложка профиля",
-    "createdAt":new Date(),
-    "updatedAt":new Date()
-  },
-  {
-    "id": 2,
-    "name": "Вложение",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  },
-  {
-    "id": 3,
-    "name": "Аватар пользователя",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  },
-  {
-    "id": 4,
-    "name": "Обложка публикации",
-    "createdAt": new Date(),
-    "updatedAt": new Date()
-  }
-], {});
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert('type_files', [
+            {
+                "id": 1,
+                "name": "Обложка профиля",
+                "createdAt": new Date(),
+                "updatedAt": new Date()
+            },
+            {
+                "id": 2,
+                "name": "Вложение",
+                "createdAt": new Date(),
+                "updatedAt": new Date()
+            },
+            {
+                "id": 3,
+                "name": "Аватар пользователя",
+                "createdAt": new Date(),
+                "updatedAt": new Date()
+            },
+            {
+                "id": 4,
+                "name": "Обложка публикации",
+                "createdAt": new Date(),
+                "updatedAt": new Date()
+            },
+            {
+                "id": 5,
+                "name": "Обложка тега",
+                "createdAt": new Date(),
+                "updatedAt": new Date()
+            },
+        ], {});
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('type_files', null, {});
-  }
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkDelete('type_files', null, {});
+    }
 };
