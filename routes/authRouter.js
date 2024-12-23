68d918f9fc646ef1bbf65d8eb0a1bc4d7eefd1e8
FOLLOO <nafis.safiullin2004@list.ru>
2024-12-23 16:54:27 +0300

    PUSH reset password!!!!

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,10 @@ const Router = require('express')
 const router = new Router()
 const authController = require('../controller/authController')
 
+router.post('/reset', authController.resetPassword) // RETURN TOKEN
+router.post('/check', authController.checkME) // ГДЕ СОХРАНЯТЬ?
+router.post('/reset/newPassword', authController.setNewPassword)
+
 router.post('/registration', authController.registration)
 router.post('/login', authController.login)
 router.post('/logout', authController.logout)
